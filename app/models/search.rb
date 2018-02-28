0df450bbd32a7c28c1d7d133c661836c9d7f9265
mfssaura <sauram@mindfiresolutions.com>
2018-02-28 14:12:11 +0530

    Added bootstrap and modified search controller

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,10 @@
 class Search < ActiveRecord::Base
+
+  def search_books
+    books = Book.all 
+    books = books.where("isbn LIKE ? and title LIKE ? and description LIKE ? and author LIKE ?
+                        and is_deleted = ?", "%#{isbn}","%#{title}", "%#{description}", "%#{author}", false)
+    return books
+  end
+  
 end
